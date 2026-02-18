faf89e026adedce0fc373762b8dbc89a238faa8a
Franco Vidalon <f.vidalons@gmail.com>
2026-02-18 16:41:58 -0500

    fix railway port binding

--- server.js
+++ server.js
@@ -83,7 +83,6 @@ app.post("/webhook", async (req, res) => {
 // =============================
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor corriendo en puerto", PORT);
 });
-
